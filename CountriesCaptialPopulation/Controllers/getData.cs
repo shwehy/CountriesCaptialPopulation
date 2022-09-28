34295f21295b1639c4627414731b8911e3a80467
HP <alyelshwahy@yahoo.com>
2022-09-28 16:05:49 +0200

    try external api

--- CountriesCaptialPopulation/Controllers/getData.cs
+++ CountriesCaptialPopulation/Controllers/getData.cs
@@ -1,10 +1,14 @@
 ﻿using CountriesCaptialPopulation.Data;
 using CountriesCaptialPopulation.Model;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace CountriesCaptialPopulation.Controllers
 {
     public class getData
     {
+        public string url = "https://countriesnow.space/api/v0.1/countries/population";
         public getData(CountryDbContext context)
         {
             _context = context;
@@ -12,6 +16,22 @@ namespace CountriesCaptialPopulation.Controllers
         private CountryDbContext _context;
         public async void save_Change()
         {
+            var client = new RestClient(url);
+            var request = new RestRequest(url, Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+            var result = JsonConvert.DeserializeObject<List<Country>>(response.Content);
+            for (int i  = 0; i < result.Count; i++)
+            {
+                if (_context.Entry(result[i]).State == EntityState.Modified)
+                {
+                    await _context.SaveChangesAsync();
+                    continue;
+                }
+
+
+            }
+            
+
             var country = new Country();
             country.CountryID = 1;
             country.Code = "4";
