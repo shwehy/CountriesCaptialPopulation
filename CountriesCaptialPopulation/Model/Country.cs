19c95b4072a571ef2950771bb38028962f095fbd
HP <alyelshwahy@yahoo.com>
2022-09-28 11:30:12 +0200

    Adding Country Model class

---
+++ CountriesCaptialPopulation/Model/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CountriesCaptialPopulation.Model
+{
+    public class Country
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<PopulationList> Populations { get; set; }
+
+    }
+    public class PopulationList
+    {
+        public int year;
+        public int value;
+    }
+}
